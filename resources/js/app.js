c8e54bdcebac1d3220ca110588f7403cc4056357
Svetlana <scherepa@gmail.com>
2026-07-10 17:39:54 +0300

    configurations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 // set jquery and bootstrap
 // on previous old versions used just cdn
 // here installed via: npm install jquery bootstrap @popperjs/core
-import './bootstrap';
-
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-
 import 'bootstrap';
+import '../css/app.css';
